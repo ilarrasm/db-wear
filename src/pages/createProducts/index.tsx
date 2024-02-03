f8679a5d9c2c1b459476297d17a8b308b224058b
ilarrasm <martin.ilarras1@gmail.com>
2024-02-03 20:33:26 -0300

    ss

--- src/pages/createProducts/index.tsx
+++ src/pages/createProducts/index.tsx
@@ -1,13 +1,12 @@
 import { Button } from "@mui/material";
 import { GetServerSideProps } from "next";
-import agregarProducto from "../../services/productSetServices/servicetest";
 
 const index = () => {
   return (
     <div>
       <Button
         onClick={() => {
-          agregarProducto()
+        //  agregarProducto()
         }}
       >
         Crear uno nuevo{" "}
